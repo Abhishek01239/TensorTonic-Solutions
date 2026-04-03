dbc4e9e937f58e1ab31dd6ff5d9c77418f801f7a
Jaiswar Abhishek <jaiswarabhishek12@gmail.com>
2026-04-03 21:05:06 +0530

    Update solution for auc

--- auc/auc.py
+++ auc/auc.py
@@ -4,28 +4,25 @@ def auc(fpr, tpr):
     fpr = np.array(fpr)
     tpr = np.array(tpr)
     
-    # Ensure FPR is sorted (monotonic increasing)
     if not np.all(fpr[:-1] <= fpr[1:]):
         raise ValueError("FPR values must be increasing")
     
-    # Compute trapezoidal integration
     auc = 0.0
     for i in range(len(fpr) - 1):
-        width = fpr[i+1] - fpr[i]           # ΔFPR
-        height = 0.5 * (tpr[i] + tpr[i+1]) # Average TPR
+        width = fpr[i+1] - fpr[i]           
+        height = 0.5 * (tpr[i] + tpr[i+1]) 
         auc += width * height
     
     return auc
 
-# ----------------- Example Usage -----------------
 fpr1 = [0, 0, 1]
 tpr1 = [0, 1, 1]
-print(auc(fpr1, tpr1))  # Output: 1.0
+print(auc(fpr1, tpr1))  
 
 fpr2 = [0, 1]
 tpr2 = [0, 1]
-print(auc(fpr2, tpr2))  # Output: 0.5
+print(auc(fpr2, tpr2))  
 
 fpr3 = [0, 0.1, 0.3, 0.6, 1.0]
 tpr3 = [0, 0.25, 0.65, 0.85, 0.95]
-print(auc(fpr3, tpr3))  # Output: 0.79
+print(auc(fpr3, tpr3))  
