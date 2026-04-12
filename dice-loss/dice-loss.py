18f5bf3393c8d4931d737e6040a7e9b91a5c3d96
Jaiswar Abhishek <jaiswarabhishek12@gmail.com>
2026-04-12 22:06:00 +0530

    Add solution for dice-loss

---
+++ dice-loss/dice-loss.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def dice_loss(p, y, eps=1e-8):
+    p = np.asarray(p, dtype=np.float32)
+    y = np.asarray(y, dtype=np.float32)
+    
+    p = p.flatten()
+    y = y.flatten()
+    
+    intersection = np.sum(p * y)
+    
+    sum_p = np.sum(p)
+    sum_y = np.sum(y)
+    
+    dice = (2 * intersection + eps) / (sum_p + sum_y + eps)
+    
+    return 1 - dice
