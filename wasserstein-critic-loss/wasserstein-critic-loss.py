23dd86d75d6aa325170006f8ee51b7575db1c125
Jaiswar Abhishek <jaiswarabhishek12@gmail.com>
2026-04-14 21:34:02 +0530

    Update solution for wasserstein-critic-loss

--- wasserstein-critic-loss/wasserstein-critic-loss.py
+++ wasserstein-critic-loss/wasserstein-critic-loss.py
@@ -3,10 +3,8 @@ import numpy as np
 def wasserstein_critic_loss(real_scores: np.ndarray, fake_scores: np.ndarray) -> float:
     fake_mean = np.mean(fake_scores)
     
-    # Compute mean of real scores
     real_mean = np.mean(real_scores)
     
-    # Wasserstein critic loss
     loss = fake_mean - real_mean
     
     return loss
