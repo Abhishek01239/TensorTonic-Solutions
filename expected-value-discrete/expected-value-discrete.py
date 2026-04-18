68dd6894e9d02fa18cdec403d509c9b1499f6394
Jaiswar Abhishek <jaiswarabhishek12@gmail.com>
2026-04-18 23:45:31 +0530

    Update solution for expected-value-discrete

--- expected-value-discrete/expected-value-discrete.py
+++ expected-value-discrete/expected-value-discrete.py
@@ -5,9 +5,10 @@ def expected_value_discrete(x, p):
     p = np.array(p, dtype=float)
     
     if x.shape != p.shape:
-        raise ValueError("x and p must have same shape")
+        raise ValueError("x and p must have the same shape")
     
     if not np.isclose(np.sum(p), 1.0, atol=1e-6):
         raise ValueError("Probabilities must sum to 1")
     
+    # Expected value
     return float(np.sum(x * p))
