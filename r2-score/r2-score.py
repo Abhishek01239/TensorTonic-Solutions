8ac4dcf1ae84a15ea3b289ccfe085f373db75dc2
Jaiswar Abhishek <jaiswarabhishek12@gmail.com>
2026-04-22 18:40:38 +0530

    Update solution for r2-score

--- r2-score/r2-score.py
+++ r2-score/r2-score.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-def r2_score(y_true, y_pred) -> float:
-    y_true = np.array(y_true)
-    y_pred = np.array(y_pred)
-
+def r2_score(y_true, y_pred):
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+    
     if np.all(y_true == y_true[0]):
-        return 1.0 if np.all(y_true == y_pred) else 0.0
-    y_bar = np.mean(y_true)
-    r2 = 1 - (np.sum((y_true - y_pred)**2))/ (np.sum((y_true - y_bar)**2))
-
-    return float(r2)
+        return 1.0 if np.allclose(y_true, y_pred) else 0.0
+    
+    y_mean = np.mean(y_true)
+    ss_tot = np.sum((y_true - y_mean) ** 2)
+    ss_res = np.sum((y_true - y_pred) ** 2)
+    
+    return float(1 - (ss_res / ss_tot))
