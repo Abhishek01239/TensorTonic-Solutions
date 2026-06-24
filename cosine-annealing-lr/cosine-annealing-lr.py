0f18a5f4506f8bfddf5ebae0d86b42d9be44e67b
Jaiswar Abhishek <jaiswarabhishek12@gmail.com>
2026-06-24 22:29:34 +0530

    Solve cosine-annealing-lr

---
+++ cosine-annealing-lr/cosine-annealing-lr.py
@@ -0,0 +1,5 @@
+import math
+def cosine_annealing_schedule(base_lr, min_lr, total_steps, current_step):
+    lr = min_lr + (1/2) * (base_lr - min_lr)*(1+math.cos((math.pi*current_step)/ total_steps))
+    return lr
+   
